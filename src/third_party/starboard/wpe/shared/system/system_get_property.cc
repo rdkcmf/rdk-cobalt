338510c0219558fa342bd8311be2f22b60e15667
Ievgen Mutavchi <Ievgen_Mutavchi@comcast.com>
2020-03-04 18:04:03 -0500

    Add option to configure model/brand values
    Change-Id: I2b67402a4e19a485b8dde5a6aa369eed5779e3ad

--- src/third_party/starboard/wpe/shared/system/system_get_property.cc
+++ src/third_party/starboard/wpe/shared/system/system_get_property.cc
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <cstdio>
+#include <cstring>
 #include <string>
 
 #if SB_API_VERSION >= 11
@@ -33,6 +35,55 @@ bool CopyStringAndTestIfSuccess(char* out_value,
   return true;
 }
 
+bool TryReadModelNameFromPropertiesFile(char* out_value, int value_length) {
+  const char kPrefixStr[] = "MODEL_NUM=";
+  const size_t kPrefixStrLength = SB_ARRAY_SIZE(kPrefixStr) - 1;
+
+  FILE* properties = fopen("/etc/device.properties", "r");
+  if (!properties) {
+    return false;
+  }
+
+  bool result = false;
+  char* buffer = nullptr;
+  size_t size = 0;
+
+  while (getline(&buffer, &size, properties) != -1) {
+    if (SbStringCompare(kPrefixStr, buffer, kPrefixStrLength) == 0) {
+      const char* remainder = buffer + kPrefixStrLength;
+      size_t remainder_length = SbStringGetLength(remainder);  // includes the newline character
+      if (remainder_length > 1 && remainder_length < value_length) {
+        SbStringCopy(out_value, remainder, remainder_length - 1);
+        result = true;
+        break;
+      }
+    }
+  }
+
+  free(buffer);
+  fclose(properties);
+
+  return result;
+}
+
+bool GetModelName(char* out_value, int value_length) {
+  const char* env = std::getenv("COBALT_MODEL_NAME");
+  if (env && CopyStringAndTestIfSuccess(out_value, value_length, env))
+    return true;
+
+  if (TryReadModelNameFromPropertiesFile(out_value, value_length))
+    return true;
+
+  return CopyStringAndTestIfSuccess(out_value, value_length, SB_PLATFORM_MODEL_NAME);
+}
+
+bool GetBrandName(char* out_value, int value_length) {
+  const char* env = std::getenv("COBALT_BRAND_NAME");
+  if (env && CopyStringAndTestIfSuccess(out_value, value_length, env))
+    return true;
+  return CopyStringAndTestIfSuccess(out_value, value_length, SB_PLATFORM_BRAND_NAME);
+}
+
 }  // namespace
 
 bool SbSystemGetProperty(SbSystemPropertyId property_id,
@@ -43,34 +94,44 @@ bool SbSystemGetProperty(SbSystemPropertyId property_id,
   }
 
   switch (property_id) {
+    case kSbSystemPropertyModelName:
+      return GetModelName(out_value, value_length);
+
     case kSbSystemPropertyBrandName:
-    return CopyStringAndTestIfSuccess(out_value, value_length,
-                                      SB_PLATFORM_OPERATOR_NAME);
+      return GetBrandName(out_value, value_length);
+
+#if defined(SB_PLATFORM_CHIPSET_MODEL_NUMBER_STRING)
     case kSbSystemPropertyChipsetModelNumber:
       return CopyStringAndTestIfSuccess(
             out_value, value_length, SB_PLATFORM_CHIPSET_MODEL_NUMBER_STRING);
+#endif  // defined(SB_PLATFORM_CHIPSET_MODEL_NUMBER_STRING)
+
+#if defined(SB_PLATFORM_FIRMWARE_VERSION_STRING)
     case kSbSystemPropertyFirmwareVersion:
       return CopyStringAndTestIfSuccess(out_value, value_length,
                                         SB_PLATFORM_FIRMWARE_VERSION_STRING);
-    case kSbSystemPropertyModelName:
-      return CopyStringAndTestIfSuccess(out_value, value_length,
-                                        SB_PLATFORM_MODEL_NAME);
+#endif  // defined(SB_PLATFORM_FIRMWARE_VERSION_STRING)
+
+#if defined(SB_PLATFORM_MODEL_YEAR)
     case kSbSystemPropertyModelYear:
       return CopyStringAndTestIfSuccess(out_value, value_length,
           std::to_string(SB_PLATFORM_MODEL_YEAR).c_str());
-#if SB_API_VERSION >= 11
+#endif  // defined(SB_PLATFORM_MODEL_YEAR)
+
+#if defined(SB_PLATFORM_MANUFACTURER_NAME)
     case kSbSystemPropertyOriginalDesignManufacturerName:
-#else
-    case kSbSystemPropertyNetworkOperatorName:
-#endif
       return CopyStringAndTestIfSuccess(out_value, value_length,
                                         SB_PLATFORM_MANUFACTURER_NAME);
+#endif  // defined(SB_PLATFORM_MANUFACTURER_NAME)
+
     case kSbSystemPropertySpeechApiKey:
       return false;
 
+#if defined(SB_PLATFORM_FRIENDLY_NAME)
     case kSbSystemPropertyFriendlyName:
       return CopyStringAndTestIfSuccess(out_value, value_length,
                                         SB_PLATFORM_FRIENDLY_NAME);
+#endif  // defined(SB_PLATFORM_FRIENDLY_NAME)
 
     case kSbSystemPropertyPlatformName:
       return CopyStringAndTestIfSuccess(out_value, value_length, kPlatformName);
