0351978a98130f782e239d63691b9b4ee030dc9b
Ievgen Mutavchi <Ievgen_Mutavchi@comcast.com>
2020-05-06 13:12:12 -0400

    Enable crash signal handlers
    Change-Id: I02135320b186e21e23ad6d9be78f50f6f644df1f

--- src/third_party/starboard/wpe/shared/main_wpe.cc
+++ src/third_party/starboard/wpe/shared/main_wpe.cc
@@ -1,3 +1,20 @@
+// If not stated otherwise in this file or this component's license file the
+// following copyright and licenses apply:
+//
+// Copyright 2020 RDK Management
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
 // Copyright 2016 The Cobalt Authors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -27,11 +44,11 @@ extern "C" SB_EXPORT_PLATFORM int main(int argc, char** argv) {
   gst_init_check(NULL, NULL, &error);
   g_free(error);
 
-  // starboard::shared::signal::InstallCrashSignalHandlers();
+  starboard::shared::signal::InstallCrashSignalHandlers();
   starboard::shared::signal::InstallSuspendSignalHandlers();
   third_party::starboard::wpe::shared::Application application;
   int result = application.Run(argc, argv);
-  // starboard::shared::signal::UninstallCrashSignalHandlers();
+  starboard::shared::signal::UninstallCrashSignalHandlers();
   starboard::shared::signal::UninstallSuspendSignalHandlers();
   return result;
 }
