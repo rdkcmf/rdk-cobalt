49c5494f3e4833fe09f9869ef00a8f6e5ce42f0e
Ravish Tayal <Ravish.Tayal@cognizant.com>
2020-09-16 19:45:46 +0000

    BLDK-742:License header updated for Cobalt
    Reason for change: Notice, Licesne header updates for open source
    Test Procedure: Jenkin build should pass after License header changes for RDK for listed repos
    BLDK-742:License header updated for Cobalt
    Risks: None
    
    Change-Id: I85534704cb0bd13c7998b129f008d98c080c3f41
    Signed-off-by: Ravish Tayal <Ravish.Tayal@cognizant.com>

--- src/third_party/starboard/rdk/shared/libcobalt.cc
+++ src/third_party/starboard/rdk/shared/libcobalt.cc
@@ -1,7 +1,5 @@
-// If not stated otherwise in this file or this component's license file the
-// following copyright and licenses apply:
 //
-// Copyright 2020 RDK Management
+// Copyright 2020 Comcast Cable Communications Management, LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,6 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
+// SPDX-License-Identifier: Apache-2.0
 
 #include "third_party/starboard/rdk/shared/libcobalt.h"
 
